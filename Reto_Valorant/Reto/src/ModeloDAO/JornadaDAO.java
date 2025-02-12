d0125e725d0ecd617b00c5f3e85c3d11747cfe8e
DaniTo <dani.tomicic@ikasle.egibide.org>
2025-02-12 12:04:41 +0100

    a

--- Reto_Valorant/Reto/src/ModeloDAO/JornadaDAO.java
+++ Reto_Valorant/Reto/src/ModeloDAO/JornadaDAO.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class JornadaDAO {
     private static ArrayList<Jornada> jornadas;
+
     public static void anadirJornada(Jornada j) {
         try {
             jornadas.add(j);
