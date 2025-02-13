502c56ae7bcfaf8981eeec582f36476a71947246
eathan <eathanandres.garzon@ikasle.egibide.org>
2025-02-13 13:01:45 +0100

    Objetos nuevos añadidos

---
+++ Reto_Valorant/Reto/src/Modelo/Competicion.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+public class Competicion {
+
+    private int codCompeticion;
+    private char estado;
+
+    public Competicion() {
+    }
+
+    public Competicion(int codCompeticion, char estado) {
+        this.codCompeticion = codCompeticion;
+        this.estado = estado;
+    }
+
+    public int getCodCompeticion() {
+        return codCompeticion;
+    }
+
+    public void setCodCompeticion(int codCompeticion) {
+        this.codCompeticion = codCompeticion;
+    }
+
+    public char getEstado() {
+        return estado;
+    }
+
+    public void setEstado(char estado) {
+        this.estado = estado;
+    }
+}
